10c42598d21132e14f37c56f1e9a2b7bfca5d7f9
smf9749 <smf9749@W16241162.ad.sprint.com>
2017-08-07 15:46:20 -0500

    added lock file to protect from running more than one per conf file

--- src/main/java/org/krawn/KrawnManager.java
+++ src/main/java/org/krawn/KrawnManager.java
@@ -1,5 +1,9 @@
 package org.krawn;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -21,8 +25,8 @@ public class KrawnManager {
 
     static ArrayList<CronJobConfig> list = new ArrayList<>();
     static long lastmod = 0L;
-    static long reaperPollTime=1000L;
-    static long configPollTime=500L;
+    static long reaperPollTime = 1000L;
+    static long configPollTime = 500L;
 
     static public class JobInfoTrack {
         public StartedProcess startedProc;
@@ -40,78 +44,95 @@ public class KrawnManager {
     static TreeMap<String, JobInfoTrack> running = new TreeMap<>();
 
     public static String[] cmd_setup;
-    
+
     static void usage() {
         System.err.println("usage: ");
-        System.err.println("nohup java -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=16m -Xloggc:log/gc.log -verbose:gc -XX:+PrintGCDateStamps -Xmx128m -jar krawn.jar krawn.conf > it 2>&1 & ");
+        System.err.println(
+                "nohup java -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=16m -Xloggc:log/gc.log -verbose:gc -XX:+PrintGCDateStamps -Xmx128m -jar krawn.jar krawn.conf > it 2>&1 & ");
         System.exit(1);
     }
     // nohup java -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=16m -Xloggc:log/gc.log -verbose:gc -XX:+PrintGCDateStamps -Xmx128m -jar krawn.jar > it 2>&1 &
+
+    static FileOutputStream fis = null;
+    static FileLock filelock = null;
+
     public static void main(String[] args) {
         try {
-        if ( args.length != 1 ) {
-            usage();
-        }
-        
-        if ( System.getProperty("os.name").toLowerCase().contains("win") ) {
-            cmd_setup = new String[] {"cmd.exe", "/C"};
-        } else {
-            cmd_setup = new String[] {"/bin/bash", "-c"};
-        }
-        
-        log.info(System.getProperty("os.name"));
-        
-        
-        if ( !Files.exists(Paths.get(args[0])) ) {
-            System.err.println("Cannot find config file: " + args[0]);
-            usage();
-        }
-        ReadConfigThread readerthread = new ReadConfigThread(args[0]);
-
-        readerthread.updateConfig();
-        
-        final AtomicBoolean keepRunning = new AtomicBoolean(true);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                log.warn("got shutdown signal");
-                keepRunning.set(false);
+            if (args.length != 1) {
+                usage();
             }
-        });
-        log.info("STARTING krawn");
-        readerthread.setDaemon(true);
-        readerthread.start();
 
-        KrawnThread krawnthread = new KrawnThread();
-        krawnthread.setDaemon(true);
-        krawnthread.start();
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                cmd_setup = new String[] { "cmd.exe", "/C" };
+            } else {
+                cmd_setup = new String[] { "/bin/bash", "-c" };
+            }
 
-        ReaperThread reaperThread = new ReaperThread(reaperPollTime);
-        reaperThread.setDaemon(true);
-        reaperThread.start();
+            log.info(System.getProperty("os.name"));
 
-        SysOutOverSLF4J.sendSystemOutAndErrToSLF4J();
-
-
-        while (keepRunning.get()) {
-            // log.info("tick");
+            if (!Files.exists(Paths.get(args[0]))) {
+                System.err.println("Cannot find config file: " + args[0]);
+                usage();
+            }
             try {
-                Thread.sleep(1000L);
-            } catch (InterruptedException e) {
-                log.warn("Shutting down...");
-                break;
+                File lockfilename = new File(args[0] + ".lock"); 
+                fis = new FileOutputStream(lockfilename, true);
+                FileChannel chan = fis.getChannel();
+                filelock = chan.tryLock();
+                if (filelock == null) {
+                    System.err.println("Unable to get exclusive ownership of the configuration sister file " + lockfilename + " - this is probably because ANOTHER INSTANCE OF THIS SAME PROGRAM IS RUNNING");
+                    usage();
+                }
 
+                ReadConfigThread readerthread = new ReadConfigThread(args[0]);
+
+                readerthread.updateConfig();
+
+                final AtomicBoolean keepRunning = new AtomicBoolean(true);
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    @Override
+                    public void run() {
+                        log.warn("got shutdown signal");
+                        keepRunning.set(false);
+                    }
+                });
+                log.info("STARTING krawn");
+                readerthread.setDaemon(true);
+                readerthread.start();
+
+                KrawnThread krawnthread = new KrawnThread();
+                krawnthread.setDaemon(true);
+                krawnthread.start();
+
+                ReaperThread reaperThread = new ReaperThread(reaperPollTime);
+                reaperThread.setDaemon(true);
+                reaperThread.start();
+
+                SysOutOverSLF4J.sendSystemOutAndErrToSLF4J();
+
+                while (keepRunning.get()) {
+                    // log.info("tick");
+                    try {
+                        Thread.sleep(1000L);
+                    } catch (InterruptedException e) {
+                        log.warn("Shutting down...");
+                        break;
+
+                    }
+                }
+                try {
+                    log.info("interupting children");
+                    readerthread.interrupt();
+                    krawnthread.interrupt();
+                } catch (Throwable ee) {
+                    log.error("error during shutdown so exit(1)", ee);
+                    System.exit(1);
+                }
+            } finally {
+                filelock.release();
             }
-        }
-        try {
-            log.info("interupting children");
-            readerthread.interrupt();
-            krawnthread.interrupt();
-        } catch (Throwable ee) {
-            log.error("error during shutdown so exit(1)", ee);
-            System.exit(1);
-        }
-        } catch(Exception e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
